6fff94016d312c7bf32a05d922bd88e9bdcf7092
KimChiXMa <23211tt4317@mail.tdc.edu.vn>
2024-11-07 14:45:02 +0700

    item to items

--- models/item.php
+++ models/item.php
@@ -1,15 +1,18 @@
 <?php
-class item extends Db{
-    public function getAllItem(){
-        $sql = self::$connection -> prepare("SELECT * FROM item");
+class item extends Db
+{
+    public function getAllItem()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM items");
         $sql->execute();
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items;
     }
-    public function getNewItem($start, $len){
-        $sql = self::$connection -> prepare("SELECT * FROM item ORDER BY `created_at` DESC LIMIT ?,?");
-        $sql->bind_param("ii",$start,$len);
+    public function getNewItem($start, $len)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM items ORDER BY `created_at` DESC LIMIT ?,?");
+        $sql->bind_param("ii", $start, $len);
         $sql->execute();
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
